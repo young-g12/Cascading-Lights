206976fa23a6b7596d72c54662e47f876cb11070
Gil <gmiranda540@yahoo.com>
2025-12-26 09:42:19 -0500

    added two more leds in array

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,8 @@
 
 const int buttonPin = 2;
 
-// LED pins stored in an array (professional style)
-const int leds[] = {4, 8, 5};
-const int numLeds = 3;
+const int leds[] = {4, 8, 11, 12};
+const int numLeds = 4;
 
 int currentLed = 0;
 bool lastButtonState = HIGH;
@@ -21,20 +20,19 @@ void setup() {
 void loop() {
   bool buttonState = digitalRead(buttonPin);
 
-  // Detect button press (HIGH → LOW)
+
   if (lastButtonState == HIGH && buttonState == LOW) {
     currentLed++;
 
     if (currentLed >= numLeds) {
-      currentLed = 0; // wrap around
+      currentLed = 0;
     }
 
-    // Turn all LEDs off
     for (int i = 0; i < numLeds; i++) {
       digitalWrite(leds[i], LOW);
     }
 
-    // Turn current LED on
+
     digitalWrite(leds[currentLed], HIGH);
   }
 
